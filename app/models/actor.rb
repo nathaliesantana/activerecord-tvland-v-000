dce8549406bbb934867376abb5cea5fa413dc276
Nathalie Santana <nathalie.scruz@gmail.com>
2018-11-11 05:10:39 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,9 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    "#{characters.name} - #{characters.show.name}"
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    ends
   end
 
 end
